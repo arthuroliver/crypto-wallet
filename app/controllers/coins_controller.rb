f5c1451baf65b5662074570ce887c562c70d6e99
arthuroliver <arthur1606@gmail.com>
2022-08-03 18:52:34 -0300

    create a coins controller and delete the unused one

--- app/controllers/coins_controller.rb
+++ app/controllers/coins_controller.rb
@@ -1,4 +1,5 @@
 class CoinsController < ApplicationController
+  before_action :set_coin, only: %i[ edit destroy update show ]
   def index
     @coins = Coin.all
   end
@@ -8,6 +9,36 @@ class CoinsController < ApplicationController
   end
 
   def create
-    @coin = Coin.new
+    @coin = Coin.new(coin_params)
+
+    if @coin.save
+      redirect_to @coin
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
+
+  def update
+    if @coin.update(coin_params)
+      redirect_to @coin
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @coin.destroy
+    redirect_to :coins, notice: "Deleted with success!"
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_coin
+      @coin = Coin.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def coin_params
+      params.require(:coin).permit(:description, :acronym, :url_image)
+    end
 end
