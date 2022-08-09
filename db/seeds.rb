3d93838548ee59b82e3ea7c53522ad8ab54ea44f
arthuroliver <arthur1606@gmail.com>
2022-08-09 11:20:32 -0300

    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Create.coin!(
+  description: "Bitcion",
+  acronym: "BTC",
+  url_iage: "https://images-americanas.b2w.io/produtos/01/00/img/1990152/9/1990152993_1GG.jpg"
+)
+
+Create.coin!(
+  description: "Ethereum",
+  acronym: "ETH",
+  url_iage: "https://d33wubrfki0l68.cloudfront.net/fcd4ecd90386aeb50a235ddc4f0063cfbb8a7b66/4295e/static/bfc04ac72981166c740b189463e1f74c/40129/eth-diamond-black-white.jpg"
+)
