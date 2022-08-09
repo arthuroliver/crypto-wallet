a2b74f236a50a43644d36bc23698b880a8ca40d7
arthuroliver <arthur1606@gmail.com>
2022-08-09 20:01:04 -0300

    config coin association

--- app/controllers/coins_controller.rb
+++ app/controllers/coins_controller.rb
@@ -1,5 +1,7 @@
 class CoinsController < ApplicationController
   before_action :set_coin, only: %i[ edit destroy update show ]
+  before_action :set_mining_type_options, only: %i[ new edit update ]
+
   def index
     @coins = Coin.all
   end
@@ -32,6 +34,10 @@ class CoinsController < ApplicationController
   end
 
   private
+    def set_mining_type_options
+      @mining_type_options = MiningType.all.pluck(:description, :id)
+    end
+
     # Use callbacks to share common setup or constraints between actions.
     def set_coin
       @coin = Coin.find(params[:id])
@@ -39,6 +45,6 @@ class CoinsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def coin_params
-      params.require(:coin).permit(:description, :acronym, :url_image)
+      params.require(:coin).permit(:description, :acronym, :url_image, :mining_type_id)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_09_145832) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_09_205544) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,6 +20,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_09_145832) do
     t.string "url_image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "mining_type_id"
+    t.index ["mining_type_id"], name: "index_coins_on_mining_type_id"
   end
 
   create_table "mining_types", force: :cascade do |t|
@@ -29,4 +31,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_09_145832) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "coins", "mining_types"
 end
