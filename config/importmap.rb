1bf932cec3bbbfafe83d0d217c13113283c4f41b
arthuroliver <arthur1606@gmail.com>
2022-08-11 16:14:18 -0300

    adding bootstrap and js

--- config/importmap.rb
+++ config/importmap.rb
@@ -5,3 +5,7 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
+pin "jquery", to: "./node_modules/jquery"
+pin "notify", to: "./node_modules/notify-js-legacy/notify"
+pin "bootstrap", to: "./node_modules/bootstrap/dist/js/bootstrap"
+pin "popper.js", to: "./node_modules/popper.js/dist/popper"
